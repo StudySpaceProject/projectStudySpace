061fc6568f7ec74d5a272e9ccda97dd6058faf15
Juan Ruiz <jdruiz008@gmail.com>
2025-10-06 02:40:49 -0500

    changes to deploy

--- src/config.ts
+++ src/config.ts
@@ -1,1 +1,3 @@
-export const API_URL = "https://studyspaceapi-production.up.railway.app/api";
+export const API_URL =
+  process.env.REACT_APP_API_URL ||
+  "https://studyspaceapi-production.up.railway.app/api";
